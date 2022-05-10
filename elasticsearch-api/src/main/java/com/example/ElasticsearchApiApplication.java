d7e31979e9c2a9c7245f1884e5271c5879afe2e5
javalover-design <2279818505@qq.com>
2022-05-10 21:06:58 +0800

    create Index

---
+++ elasticsearch-api/src/main/java/com/example/ElasticsearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchApiApplication.class, args);
+    }
+
+}
